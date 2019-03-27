222b21e771aa438e735a49d65027801d1f37562c
lukiguki <31853688+lukiguki@users.noreply.github.com>
2019-03-27 13:25:42 +0100

    start

--- index.php
+++ index.php
@@ -5,11 +5,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.scss">
 </head>
 
 <body>
     <h1>Det Funkar!</h1>
 </body>
+    <div class="container">
+        <header>
+            <h1>ElevRate</h1>
+            <nav>
+                <a href="#">HOME</a>
+                <a href="#">REVIEWS</a>
+                <a href="#">SIGNUP</a>
+                <a href="#">LOGIN</a>
+            </nav>
+        </header>
+        <main>
+            <input name="search" type="text">
+            <button>Search</button>
+            <section>
+                <img src="Images/map.png" alt="">
+            </section>
+        </main>
+    </div>
 
 </html>
