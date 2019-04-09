01d59664781035bb2290f6bf1c8c895df8bf528d
lukiguki <31853688+lukiguki@users.noreply.github.com>
2019-04-09 14:44:26 +0200

    börjat med php för formuläret

---
+++ config-db.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "localhost";
+$database = "elevrate";
+$user = "elevrate";
+$password = "Gn9Ag2wE51h5mNXr";
+?>
--- signup.php
+++ signup.php
@@ -1,3 +1,17 @@
+<?php
+/*
+* PHP version 7
+* @category   Signup system
+* @author     Lukas Kirby <lukas.kirby@hotmail.com>
+* @license    PHP CC
+*/
+
+/* aktivera felmeddelanden */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+include_once "./config-db.php";
+?>
 <!DOCTYPE html>
 <html lang="sv">
 
@@ -11,24 +25,57 @@
 <body>
     <div class="container">
         <header>
-        <h1>ElevRate</h1>
+            <h1>ElevRate</h1>
             <nav>
-                <a href="index.php">HOME</a><a href="reviews.php">REVIEWS</a><a href="#" class="active">SIGNUP</a><a href="login.php">LOGIN</a>
+                <a href="index.php">HOME</a><a href="reviews.php">REVIEWS</a><a href="#" class="active">SIGNUP</a><a
+                    href="login.php">LOGIN</a>
             </nav>
         </header>
         <main>
             <section>
-                <label for="fornamn">Förnamn:</label><br>
-                <input name="fornamn" type="text" class="form"><br>
-                <label for="efternamn">Efternamn:</label><br>
-                <input name="efternamn" type="text" class="form"><br>
-                <label for="losenord">Lösenord:</label><br>
-                <input name="losenord" type="text" class="form"><br>
-                <label for="replosen">Repetera Lösenord:</label><br>
-                <input name="replosen" type="text" class="form"><br>
-                <label for="email">E-Mail:</label><br>
-                <input name="email" type="text" class="form"><br>
-                <button>Registrera</button>
+                <form action="#" method="post">
+                    <label for="fornamn">Förnamn:</label><br>
+                    <input name="fornamn" type="text" class="form"><br>
+                    <label for="efternamn">Efternamn:</label><br>
+                    <input name="efternamn" type="text" class="form"><br>
+                    <label for="losenord">Lösenord:</label><br>
+                    <input name="losenord" type="text" class="form"><br>
+                    <label for="replosen">Repetera Lösenord:</label><br>
+                    <input name="replosen" type="text" class="form"><br>
+                    <label for="email">E-Mail:</label><br>
+                    <input name="email" type="text" class="form"><br>
+                    <button>Registrera</button>
+                </form>
+                <?php
+                if (isset($_POST["fornamn"]) && isset($_POST["efternamn"]) && isset($_POST["losenord"]) && isset($_POST["replosen"]) &&isset($_POST["email"])){
+                    $fnamn = filter_input(INPUT_POST, "fornamn", FILTER_SANITIZE_STRING);
+                    $enamn = filter_input(INPUT_POST, "efternamn", FILTER_SANITIZE_STRING);
+                    $losen = filter_input(INPUT_POST, "losenord", FILTER_SANITIZE_STRING);
+                    $rlosen = filter_input(INPUT_POST, "replosen", FILTER_SANITIZE_STRING);
+                    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
+
+                    $conn = new mysqli($hostname, $user, $password, $database);
+
+                    /* kolla att vi har en fungerande anslutning */
+                    if ($conn->connect_error) {
+                        die("Kunde inte ansluta till databasen: " . $conn->connect_error);
+                    }
+
+                    /* räkna fram hashet på lösenordet */
+                    $hash = password_hash($losen, PASSWORD_DEFAULT);
+
+                    /* Anslutningen fungerar. Nu skjuter vi in data i tabellen */
+                    $sql = "INSERT INTO admin (fornamn, efternamn, epost, losen) VALUES ('$fnamn', '$enamn', '$email', '$hash');";
+                    $result = $conn->query($sql);
+
+                    /* kunde sql satsen köras */
+                    if (!$result) {
+                        die("något blev fel med sql satsen");
+                    }
+                    
+                    
+                }
+                ?>
             </section>
         </main>
     </div>
